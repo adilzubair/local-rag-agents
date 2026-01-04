263f22612eea7b36f2b501114c8fe933791005b6
Muhamed Adil <muhamedadil03@gmail.com>
2026-01-04 20:27:10 +0400

    added memory and structured response format

--- langchain-basics/main.py
+++ langchain-basics/main.py
@@ -20,9 +20,9 @@ class ResponseFormat:
     temperature_fahrenheit: float
     humidity: float
 
+model = init_chat_model('gpt-5-nano', temperature=0.7)
 
-
-
+checkpointer = InMemorySaver()
 
 
 WEATHER_API = os.getenv("WEATHER_API_KEY")
@@ -35,19 +35,45 @@ def get_weather(city: str):
     
     data = response.json()
     return data
+
+@tool('locate_user', description='Look up a users city based on context')
+def locate_user(runtime: ToolRuntime[Context]):
+    match runtime.context.user_id:
+        case 'ABC123':
+            return 'Vienna'
+        case 'XYZ456':
+            return 'Paris'
+        case _:
+            return 'Unknown'
     
 
 agent = create_agent(
-    model='gpt-5-nano',
-    tools = [get_weather],
-    system_prompt='you are a weather assistant who always cracks jokes and is humorous while remaining helpful'
+    model= model,
+    tools = [get_weather, locate_user],
+    system_prompt='you are a weather assistant who always cracks jokes and is humorous while remaining helpful',
+    context_schema=Context,
+    response_format=ResponseFormat,
+    checkpointer=checkpointer 
 )
+
+config = {'configurable': {'thread_id': 1}}
 
 response = agent.invoke({
     'messages': [
-        {'role': 'user', 'content': 'what is the weather like in dubai?'} 
-    ]
-})
+        {'role': 'user', 'content': 'what is the weather like?'} 
+    ]},
+    config = config,                                #type: ignore
+    context = Context(user_id='ABC123')
+)
 
-print(response)
-print(response['messages'][-1].content)
+print(response['structured_response'])
+
+response = agent.invoke({
+    'messages': [
+        {'role': 'user', 'content': 'And is this usual?'} 
+    ]},
+    config = config,                                #type: ignore
+    context = Context(user_id='ABC123')
+)
+
+print(response['structured_response'].summary)
